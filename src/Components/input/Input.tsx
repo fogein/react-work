950eb44dc4afac80dc80a35ef6d085077a6ca06d
Fogein <fogein3311@gmail.com>
2021-09-27 23:31:04 +0300

    work on validation and created universal input

--- src/Components/input/Input.tsx
+++ src/Components/input/Input.tsx
@@ -1,14 +1,37 @@
 import React from 'react'
+let classes = require( './input.module.css')
 
 
-export const Input = (props:any) => {
+const isInvalid = ({valid, touched, shouldValidate}:any) =>{
+  return !valid && shouldValidate && touched
+}
+
+
+export const Input = (props:any) => { 
+  const inputType = props.type || "text"
+  const cls = [classes.Input]
+  const htmlFor = `${inputType}-${Math.random()}`
+
+  if (isInvalid(props)) {
+    cls.push(classes.invalid)
+  }
+
   return (
     <>
-        <div className="input__form">
-          <label className="label__form" htmlFor="login__default">{props.label}
-            <input type="text" className="input" id="login__default" />
-            <span className="errorMesage">{props.error}</span>
-          </label>
+        <div className={cls.join(' ')}>
+          <label className="" htmlFor={htmlFor}>{props.label}</label>
+            <input 
+            type={inputType} 
+            className="input" 
+            id={htmlFor} 
+            value={props.value}
+            onChange={props.onChange}
+            />
+            {
+              isInvalid(props)
+              ?<span>{props.errorMessage}</span>
+              :null
+            }
         </div>
     </>
   )
--- src/Pages/page_enter/SignIn.tsx
+++ src/Pages/page_enter/SignIn.tsx
@@ -1,22 +1,91 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable react/jsx-pascal-case */
-import React from 'react'
 import { Sign_In_but } from '../../Components/Buttons/SignIn/Sign_In-but'
 import { Input } from '../../Components/input/Input'
 import signInBg from '../../assets/SignInBg.png'
 
-export const SignIn = (props:any) => {
+
+export interface ISignIn {
+  props?:any
+}
+
+
+export const SignIn:React.FC = () => {
+
+ const state: any = {
+    formControls:{
+      email: {
+        value:'',
+        type:'email',
+        label:'E-mail',
+        errorMesage:'Введите коректный емаил',
+        valid:false,
+        touched:false,
+        validation:{
+          required:false,
+          email:true
+        }
+      },
+      password: {
+          value:'',
+          type:'password',
+          label:'Password',
+          errorMesage:'Введите коректный password',
+          valid:false,
+          touched:false,
+          validation:{
+            required:false,
+            minLength:6
+          }
+      }
+    }
+  }
+
+  const loginHandler = () =>{
+
+  }
+  const registerHandler = () =>{
+
+  }
+  const submitHandler = (event: { preventDefault: () => void }) =>{
+    event.preventDefault()
+  }
+  const onChangeHadler = (event: any, controlName: any) => {
+    console.log(`${controlName}:`, event.target.value)
+  }
+
+
+  const renderInputs = () => {
+    return Object.keys(state.formControls).map((controlName, index) => {
+      const control = state.formControls[controlName]
+      return (
+        <Input
+        key={controlName + index}
+        type={control.type}
+        value={control.value}
+        valid={control.valid}
+        touched={control.touched}
+        label={control.label}
+        shouldValidate={!!control.validation}
+        errorMesage={control.errorMesage}
+        onChange={(event: any) => onChangeHadler (controlName ,event) }
+        />
+      )
+    })
+  }
+
   return (
       <div className="sign_in-container">
         <div className="sign_in-form">
           <h1 className="sign_in-title">Sign In</h1>
-          <form action="">
-          <Input label="Login" />
-          <Input label="Password" />
+          <form onSubmit={submitHandler}>
+
+          {renderInputs()}
+
+          <Sign_In_but onClick={loginHandler} label="Sign In"/>
           </form>
-          <Sign_In_but label="Sign In"/>
-          <span className="sign_in-description">Not a member yet? <a href="#" className="link_signUp">Sign up</a></span>
+          <span className="sign_in-description">Not a member yet? <button onClick={registerHandler} className="link_signUp">Sign up</button></span>
 
         </div>
         <div className="sign_in-bg">
